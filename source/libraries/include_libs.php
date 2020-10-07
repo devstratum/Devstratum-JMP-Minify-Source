72dc88178618a60427f97594544f47495d946a91
Sergey Osipov <47226965+devstratum@users.noreply.github.com>
2020-10-07 19:12:34 +0300

    Add files via upload

---
+++ source/libraries/include_libs.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/minify/src/Minify.php';
+require_once __DIR__ . '/minify/src/CSS.php';
+require_once __DIR__ . '/minify/src/JS.php';
+require_once __DIR__ . '/minify/src/Exception.php';
+require_once __DIR__ . '/minify/src/Exceptions/BasicException.php';
+require_once __DIR__ . '/minify/src/Exceptions/FileImportException.php';
+require_once __DIR__ . '/minify/src/Exceptions/IOException.php';
+require_once __DIR__ . '/path-converter/src/ConverterInterface.php';
+require_once __DIR__ . '/path-converter/src/Converter.php';
